814638b75e80784303570cc04bf5594e82bfdb66
KIMSEI1124 <workju1124@gmail.com>
2023-04-25 16:31:17 +0900

    chore: 스프링 프로젝트 생성

---
+++ backend/src/main/java/com/multi/mariage/MariageApplication.java
@@ -0,0 +1,13 @@
+package com.multi.mariage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/multi/mariage/MariageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.mariage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
