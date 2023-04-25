d123d0234155b950efed34eacd688cdd649223b1
KIMSEI1124 <workju1124@gmail.com>
2023-04-25 15:52:12 +0900

    chore: Prettie 설정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,55 +1,55 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const path = require("path");
+const path = require('path');
 
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "./src/index.tsx"),
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.tsx'),
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/",
-    filename: "[contenthash].bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: '[contenthash].bundle.js',
     clean: true,
   },
   devServer: {
-    static: path.resolve(__dirname, "./public"),
+    static: path.resolve(__dirname, './public'),
     port: 3000,
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: {
-          loader: "esbuild-loader",
+          loader: 'esbuild-loader',
           options: {
-            loader: "tsx",
-            target: "es2015",
+            loader: 'tsx',
+            target: 'es2015',
           },
         },
       },
       {
         test: /\.(woff(2)?|ttf|png|jpe?g|gif|webp)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack", "file-loader"],
+        use: ['@svgr/webpack', 'file-loader'],
       },
     ],
   },
   plugins: [
     new webpack.ProvidePlugin({
-      React: "react",
+      React: 'react',
     }),
     new HtmlWebpackPlugin({
-      template: "public/index.html",
+      template: 'public/index.html',
     }),
     new CleanWebpackPlugin(),
   ],
