05a210dab7538657d5d5525a6363d6e07509f987
KIMSEI1124 <workju1124@gmail.com>
2023-04-26 10:28:55 +0900

    remove: 폰트 삭제
    SCoreDream 폰트 삭제

--- frontend/src/styles/fonts.tsx
+++ frontend/src/styles/fonts.tsx
@@ -2,14 +2,6 @@ import { css } from '@emotion/react';
 
 const fontStyle = css`
   @font-face {
-    font-family: 'SCoreDream';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff')
-      format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  @font-face {
     font-family: 'IBMPlexSans';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff')
       format('woff');
