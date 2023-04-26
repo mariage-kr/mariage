b395d15f57a969146d4e5ce8be88d109fc0f0601
KIMSEI1124 <workju1124@gmail.com>
2023-04-26 13:30:56 +0900

    chore: yml 파일 작성

--- backend/src/main/java/com/multi/mariage/weather/domain/WeatherRepository.java
+++ backend/src/main/java/com/multi/mariage/weather/domain/WeatherRepository.java
@@ -2,5 +2,8 @@ package com.multi.mariage.weather.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+    boolean existsByDate(LocalDateTime date);
 }
