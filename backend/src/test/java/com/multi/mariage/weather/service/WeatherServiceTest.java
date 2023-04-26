55d3728e4cbec5c9fb7490d6b75620bfa30d9d6a
KIMSEI1124 <workju1124@gmail.com>
2023-04-26 14:51:06 +0900

    test: 날씨 정보 저장 테스트 코드 작성

--- backend/src/test/java/com/multi/mariage/weather/service/WeatherServiceTest.java
+++ backend/src/test/java/com/multi/mariage/weather/service/WeatherServiceTest.java
@@ -1,24 +1,29 @@
 package com.multi.mariage.weather.service;
 
+import com.multi.mariage.weather.domain.Weather;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-@Rollback(value = false)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
 @SpringBootTest
 class WeatherServiceTest {
 
     @Autowired
     private WeatherService weatherService;
 
+    @DisplayName("날씨 정보를 저장한다")
     @Test
-    void save() {
+    void 날씨_정보를_저장한다() {
         // given
-        weatherService.save();
         // when
+        Weather actual = weatherService.save();
 
         // then
-
+        assertThat(actual).isNotNull();
     }
 }
