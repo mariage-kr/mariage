9d8625faa4ae52bf55808d3810a754354634c468
KIMSEI1124 <workju1124@gmail.com>
2023-04-26 10:10:13 +0900

    feat: 글로벌 스타일 지정
    font 설정

---
+++ frontend/src/index.styled.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+const IBM = styled.p`
+  font-family: 'IBMPlexSans';
+  //   font-size: 2rem;
+  font-weight: 400;
+`;
+
+const SCore = styled.p`
+  font-family: 'SCoreDream';
+  font-size: 2rem;
+`;
+
+const Neo = styled.p`
+  font-family: 'NanumSquareNeo';
+  font-size: 2rem;
+`;
+
+export { IBM, SCore, Neo };
---
+++ frontend/src/styles/global.tsx
@@ -0,0 +1,30 @@
+import { Global, css } from '@emotion/react';
+
+import { fontStyle } from './fonts';
+
+const style = () => css`
+  ${fontStyle}
+
+  body {
+    font-family: 'IBMPlexSans';
+    font-size: 1.1rem;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  span {
+    font-family: 'NanumSquareNeo';
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+function GlobalStyle() {
+  return <Global styles={style} />;
+}
+
+export default GlobalStyle;
