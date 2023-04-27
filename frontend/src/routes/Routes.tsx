9c64a70ef9dcfaae67dac86aa88ea30ea2f91a58
KIMSEI1124 <workju1124@gmail.com>
2023-04-27 15:54:57 +0900

    feat: 라우터 설정

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -9,3 +9,21 @@ import {
 } from '../pages/Pages';
 
 import { Route, Routes as BrowserRoutes } from 'react-router-dom';
+
+import { BROWSER_PATH } from '../constants/path';
+
+function Routes() {
+  return (
+    <BrowserRoutes>
+      <Route path={BROWSER_PATH.BASE} element={<Main />} />
+      <Route path={BROWSER_PATH.DETAIL} element={<Detail />} />
+      <Route path={BROWSER_PATH.MY} element={<MyInformation />} />
+      <Route path={BROWSER_PATH.PRODUCT} element={<Product />} />
+      <Route path={BROWSER_PATH.LOGIN} element={<Login />} />
+      <Route path={BROWSER_PATH.SIGN_UP} element={<SignUp />} />
+      <Route path={'*'} element={<NotFound />} />
+    </BrowserRoutes>
+  );
+}
+
+export default Routes;
