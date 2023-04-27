bea0065ab175d90b75cc6e717a560277fb329a89
KIMSEI1124 <workju1124@gmail.com>
2023-04-27 16:04:04 +0900

    feat: 라우터 글로벌 설정
    Webpack 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import Loading from './components/Loading/Loading';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
+import Routes from './routes/Routes';
+import { Suspense } from 'react';
+import Loading from './components/Loading/Loading';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Loading />
+      <Router>
+        <Suspense fallback={<Loading />}>
+          <Routes />
+        </Suspense>
+      </Router>
     </>
   );
 }
