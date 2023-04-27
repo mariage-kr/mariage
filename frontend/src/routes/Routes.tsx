c242aa41d322211c91d1abe383f3f20e16d508cf
KIMSEI1124 <workju1124@gmail.com>
2023-04-27 15:50:04 +0900

    chore: react-router-dom 추가

---
+++ frontend/src/routes/Routes.tsx
@@ -0,0 +1,11 @@
+import {
+  Detail,
+  Login,
+  Main,
+  MyInformation,
+  NotFound,
+  Product,
+  SignUp,
+} from '../pages/Pages';
+
+import { Route, Routes as BrowserRoutes } from 'react-router-dom';
