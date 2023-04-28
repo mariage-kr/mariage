253ec4061a2a57849b019c31aba221bd026b9d77
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-04-28 16:55:41 +0900

    test: 멤버 Repository 회원가입 테스트 코드 작성
    Related to: #31

---
+++ backend/src/test/java/com/multi/mariage/member/domain/MemberRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.multi.mariage.member.domain;
+
+import com.multi.mariage.member.domain.embedded.Email;
+import com.multi.mariage.member.fixture.MemberFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        member = memberRepository.save(MemberFixture.MARI.toMember());
+    }
+
+    @DisplayName("회원을 저장한다.")
+    @Test
+    void 회원을_저장한다() {
+        Member expected = MemberFixture.SURI.toMember();
+
+        Member actual = memberRepository.save(expected);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("해당 이메일이 이미 존재하는지 확인한다.")
+    @Test
+    void 해당_이메일이_이미_존재하는지_확인한다() {
+        String email = member.getEmail();
+
+        boolean actual = memberRepository.existsByEmail(Email.of(email));
+
+        assertThat(actual).isTrue();
+    }
+}
