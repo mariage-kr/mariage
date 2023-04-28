14b2dd904b9183914f9cdd4ff711c501b1c4e3f2
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-04-28 16:48:25 +0900

    test: 회원 회원가입 서비스 테스트 코드 작성

---
+++ backend/src/test/java/com/multi/mariage/member/fixture/MemberFixture.java
@@ -0,0 +1,57 @@
+package com.multi.mariage.member.fixture;
+
+import com.multi.mariage.member.domain.Gender;
+import com.multi.mariage.member.domain.Member;
+import com.multi.mariage.member.domain.embedded.Email;
+import com.multi.mariage.member.domain.embedded.Name;
+import com.multi.mariage.member.domain.embedded.Nickname;
+import com.multi.mariage.member.domain.embedded.Password;
+import com.multi.mariage.member.dto.request.MemberSignupRequest;
+import groovy.transform.builder.Builder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDate;
+
+public enum MemberFixture {
+    MARI("마리", "mari2023@gmail.com", "mari23!@", "마리", LocalDate.of(1990, 1, 1), Gender.WOMAN),
+    SURI("수리","suri2023@gmail.com", "suri23!@", "수리", LocalDate.of(1991, 12, 31), Gender.MAN);
+
+    private final String name;
+    private final String email;
+    private final String password;
+    private final String nickname;
+    private final LocalDate birth;
+    private final Gender gender;
+
+    @Builder
+    MemberFixture(String name, String email, String password, String nickname, LocalDate birth, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.birth = birth;
+        this.gender = gender;
+    }
+
+    public Member toMember() {
+        return Member.builder()
+                .name(Name.of(name))
+                .email(Email.of(email))
+                .password(Password.encrypt(password, new BCryptPasswordEncoder()))
+                .nickname(Nickname.of(nickname))
+                .birth(birth)
+                .gender(gender)
+                .build();
+    }
+
+    public MemberSignupRequest toSignupRequest() {
+        return MemberSignupRequest.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .birth(birth)
+                .gender(gender)
+                .build();
+    }
+}
---
+++ backend/src/test/java/com/multi/mariage/member/service/MemberServiceTest.java
@@ -0,0 +1,49 @@
+package com.multi.mariage.member.service;
+
+import com.multi.mariage.member.domain.Member;
+import com.multi.mariage.member.dto.request.MemberSignupRequest;
+import com.multi.mariage.member.exception.MemberErrorCode;
+import com.multi.mariage.member.exception.MemberException;
+import com.multi.mariage.member.fixture.MemberFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Transactional
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @BeforeEach
+    void setUp() {
+        memberService.signup(MemberFixture.MARI.toSignupRequest());
+    }
+
+    @DisplayName("회원 가입한다.")
+    @Test
+    void 회원_가입한다() {
+        MemberSignupRequest request = MemberFixture.SURI.toSignupRequest();
+
+        Member actual = memberService.signup(request);
+
+        assertThat(actual).isNotNull();
+    }
+
+    @DisplayName("이미 가입된 이메일인 경우 예외를 던진다.")
+    @Test
+    void 이미_가입된_이메일인_경우_예외를_던진다() {
+        MemberSignupRequest request = MemberFixture.MARI.toSignupRequest();
+
+        assertThatThrownBy(() -> memberService.signup(request))
+                .isInstanceOf(MemberException.class)
+                .hasMessageContaining(MemberErrorCode.SIGNUP_INVALID_EMAIL.getMessage());
+    }
+}
