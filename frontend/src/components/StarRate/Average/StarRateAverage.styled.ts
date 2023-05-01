e3bd06f68055c65fa0bfb507dff73cdbfb5bf577
rrimm <kmr1033@naver.com>
2023-05-01 21:00:21 +0900

    feat: 별점 컴포넌트(리뷰 통계용) 추가

---
+++ frontend/src/components/StarRate/Average/StarRateAverage.styled.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const StarSvgStyle = styled.svg`
+  width: 20px;
+  height: 20px; 
+  view-box: 0 0 20 20;
+  fill: #E9E6FF;
+`;
+
+  export {StarSvgStyle};
