40309cb9663810e6e6e24b70cc4e9d020e627c17
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-01 20:18:09 +0900

    feat: 글로벌 예외 처리 구현

--- backend/src/main/java/com/multi/mariage/member/controller/MemberController.java
+++ backend/src/main/java/com/multi/mariage/member/controller/MemberController.java
@@ -4,8 +4,10 @@ import com.multi.mariage.member.dto.request.MemberSignupRequest;
 import com.multi.mariage.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
