6c2d43b9890faa624898dcd29a92c3e724665b9d
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-01 11:30:45 +0900

    feat: 회원가입 ui 구현

--- frontend/src/pages/SignUp/SignUp.styled.ts
+++ frontend/src/pages/SignUp/SignUp.styled.ts
@@ -1,8 +1,59 @@
 import styled from '@emotion/styled';
 
-const Input = styled.input`
-  width: 100%;
-  height: 2rem;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
 
-export { Input };
+const Header = styled.h1`
+  color: black;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  flex-direction: column;
+
+  width: 460px;
+`;
+
+const Input = styled.input`
+  width: 460px;
+  height: 26px;
+
+  margin-bottom: 30px;
+
+  border: 0;
+  border-bottom: 1px solid;
+
+  font-size: 16px;
+  outline: none;
+
+  &:focus {
+    border-bottom: 1px solid #9c94d0;
+  }
+`;
+
+const Button = styled.button`
+  width: 460px;
+  height: 52px;
+
+  border-radius: 5px;
+
+  background-color: #9c94d0;
+  font-size: 20px;
+  color: #f8f8f8;
+  outline: none;
+  border: 0;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+  font-size: 13px;
+`;
+
+export { Container, Header, Wrapper, Input, Label, Button };
--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,15 +1,49 @@
-import * as S from './SignUp.styled';
 import useInput from '../../hooks/useInput';
 
+import * as S from './SignUp.styled';
+
 function SignUp() {
+  const { value: name, setValue: setName } = useInput();
   const { value: email, setValue: setEmail } = useInput();
+  const { value: password, setValue: setPassword } = useInput();
+  const { value: confirmPassword, setValue: setConfirmPassword } = useInput();
+  const { value: nickname, setValue: setNickname } = useInput();
+  const { value: birth, setValue: setBirth } = useInput();
 
   return (
-    <div>
-      <h1>회원가입 페이지</h1>
-      <S.Input type={'text'} value={email} onChange={setEmail} required />
-      <p>{email}</p>
-    </div>
+    <S.Container>
+      <S.Header>회원가입</S.Header>
+      <S.Wrapper>
+        <S.Label>이름</S.Label>
+        <S.Input type={'text'} value={name} onChange={setName} required />
+        <S.Label>이메일</S.Label>
+        <S.Input type={'text'} value={email} onChange={setEmail} required />
+        <S.Label>비밀번호</S.Label>
+        <S.Input
+          type={'password'}
+          value={password}
+          onChange={setPassword}
+          required
+        />
+        <S.Label>비밀번호 확인</S.Label>
+        <S.Input
+          type={'password'}
+          value={confirmPassword}
+          onChange={setConfirmPassword}
+          required
+        />
+        <S.Label>닉네임</S.Label>
+        <S.Input
+          type={'text'}
+          value={nickname}
+          onChange={setNickname}
+          required
+        />
+        <S.Label>생년월일</S.Label>
+        <S.Input type={'date'} value={birth} onChange={setBirth} required />
+        <S.Button>가입하기</S.Button>
+      </S.Wrapper>
+    </S.Container>
   );
 }
 
