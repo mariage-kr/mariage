ed0a05b80e8ff1eb9f9af0d6108ca0e5b7ee6745
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-03 13:23:24 +0900

    feat: 입력값이 없을 시 foucs 기능 구현

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { requestLogin } from '@/apis/request/auth';
 import { Token } from '@/types/user';
 import useAuth from '@/hooks/useAuth';
-import useInput from '@/hooks/useInput';
 import { BROWSER_PATH } from '@/constants/path';
 
 import * as S from './Login.styled';
@@ -17,29 +16,46 @@ function Login() {
   const [isValid, setIsValid] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const { value: email, setValue: setEmail } = useInput('');
-  const { value: password, setValue: setPassword } = useInput('');
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
-  const validateInput = () => {
-    if (email.length === 0) {
+  const validateInput = (
+    emailRef: React.RefObject<HTMLInputElement>,
+    passwordRef: React.RefObject<HTMLInputElement>,
+  ) => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (email?.length === 0) {
       setIsValid(true);
       setErrorMessage('이메일을 입력해주세요.');
+      emailRef.current?.focus();
       return true;
     }
-    if (password.length === 0) {
+
+    if (password?.length === 0) {
       setIsValid(true);
       setErrorMessage('비밀번호를 입력해주세요.');
+      passwordRef.current?.focus();
       return true;
     }
+
     return false;
   };
 
   const login = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (validateInput()) {
+    if (
+      !emailRef.current ||
+      !passwordRef.current ||
+      validateInput(emailRef, passwordRef)
+    ) {
       return;
     }
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
 
     requestLogin({ email, password })
       .then(response => {
@@ -64,9 +80,9 @@ function Login() {
       </S.StyledLink>
       <S.Form onSubmit={login}>
         <S.Label>이름</S.Label>
-        <S.Input type={'text'} value={email} onChange={setEmail} />
+        <S.Input type={'text'} ref={emailRef} />
         <S.Label>비밀번호</S.Label>
-        <S.Input type={'password'} value={password} onChange={setPassword} />
+        <S.Input type={'password'} ref={passwordRef} />
         {isValid && <S.ErrorMessage>{errorMessage}</S.ErrorMessage>}
         <S.Button type={'submit'}>로그인</S.Button>
       </S.Form>
