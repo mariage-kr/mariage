f6b1299b5c0a887f5ea72baaa5b45a03258db6c8
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-03 10:07:43 +0900

    test: 엑세스 토큰 재발급 테스트 코드 작성
    Resolves: #63

--- backend/src/main/java/com/multi/mariage/auth/dto/request/ReissueRequest.java
+++ backend/src/main/java/com/multi/mariage/auth/dto/request/ReissueRequest.java
@@ -1,5 +1,6 @@
 package com.multi.mariage.auth.dto.request;
 
+import com.multi.mariage.auth.dto.response.TokenResponse;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,4 +10,13 @@ import lombok.NoArgsConstructor;
 public class ReissueRequest {
     private String accessToken;
     private String refreshToken;
+
+    private ReissueRequest(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public static ReissueRequest from(TokenResponse token) {
+        return new ReissueRequest(token.getAccessToken(), token.getRefreshToken());
+    }
 }
--- backend/src/test/java/com/multi/mariage/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/multi/mariage/auth/controller/AuthControllerTest.java
@@ -1,5 +1,7 @@
 package com.multi.mariage.auth.controller;
 
+import com.multi.mariage.auth.dto.request.LoginRequest;
+import com.multi.mariage.auth.dto.request.ReissueRequest;
 import com.multi.mariage.auth.dto.response.TokenResponse;
 import com.multi.mariage.auth.service.AuthService;
 import com.multi.mariage.common.annotation.ControllerTest;
@@ -41,7 +43,7 @@ class AuthControllerTest extends ControllerTest {
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andDo(print())
                 .andDo(
-                        document("Login",
+                        document("Auth: Login",
                                 preprocessRequest(prettyPrint()),
                                 preprocessResponse(prettyPrint()),
                                 requestFields(
@@ -61,15 +63,46 @@ class AuthControllerTest extends ControllerTest {
     @DisplayName("회원이 로그아웃한다.")
     @Test
     void 회원이_로그아웃한다() throws Exception {
-        TokenResponse token = authService.login(MARI.toLoginRequest());
+        TokenResponse token = loginMember(MARI.toLoginRequest());
 
         mockMvc.perform(delete("/api/user/auth/logout")
                         .header(AUTHORIZATION, BEARER_PREFIX + token.getAccessToken()))
-                .andDo(document("Logout"))
+                .andDo(document("Auth: Logout"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 엑세스_토큰을_재발급한다() throws Exception {
+        TokenResponse token = loginMember(MARI.toLoginRequest());
+        String content = objectMapper.writeValueAsString(ReissueRequest.from(token));
+
+        mockMvc.perform(post("/api/auth/reissue")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andDo(
+                        document("Auth: Reissue",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestFields(
+                                        fieldWithPath("accessToken").description("엑세스 토큰"),
+                                        fieldWithPath("refreshToken").description("재발급 토큰")
+                                ),
+                                responseFields(
+                                        fieldWithPath("grantType").description("부여 유형"),
+                                        fieldWithPath("accessToken").description("엑세스 토큰"),
+                                        fieldWithPath("refreshToken").description("재발급 토큰"),
+                                        fieldWithPath("accessTokenExpiresIn").description("엑세스 토큰 만료일")
+                                ))
+                )
                 .andExpect(status().isOk());
     }
 
     void signupMember() {
         memberService.signup(MARI.toSignupRequest());
+    }
+
+    TokenResponse loginMember(LoginRequest request) {
+        return authService.login(request);
     }
 }
