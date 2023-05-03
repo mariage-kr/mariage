3f985160f513e0ae2e3d3043b7747849a45e19ee
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-03 10:25:44 +0900

    feat: 회원가입 UI 구현
    Related to: #65

---
+++ frontend/src/pages/Login/Login.styled.ts
@@ -0,0 +1,95 @@
+import styled from '@emotion/styled';
+
+type InfoMessageProps = {
+  isValid?: boolean;
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Header = styled.h1`
+  color: black;
+`;
+
+const Form = styled.form`
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  flex-direction: column;
+
+  width: 460px;
+`;
+
+const Input = styled.input`
+  width: 460px;
+  height: 42px;
+
+  margin-bottom: 20px;
+
+  border: 0;
+  border-bottom: 2px solid;
+
+  font-size: 16px;
+
+  transition: 100ms;
+
+  &:focus {
+    border-bottom: 2px solid #9c94d0;
+    border-radius: 5px;
+  }
+`;
+
+const Button = styled.button`
+  font-size: 20px;
+
+  width: 460px;
+  height: 52px;
+
+  background-color: #9c94d0;
+  color: #f8f8f8;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+  font-size: 14px;
+`;
+
+const InfoMessage = styled.span<InfoMessageProps>`
+  color: #bb2649;
+  visibility: ${({ isValid }) => (isValid ? 'none' : 'hidden')};
+  font-size: 0.7rem;
+  margin-bottom: 15px;
+`;
+
+const ErrorMessage = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 40px;
+  width: 460px;
+
+  border-radius: 3px;
+  background-color: #9c94d050;
+
+  color: #bb2649;
+  font-size: 0.9rem;
+
+  margin-bottom: 30px;
+`;
+
+export {
+  Container,
+  Header,
+  Form,
+  Input,
+  Label,
+  Button,
+  InfoMessage,
+  ErrorMessage,
+};
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,5 +1,28 @@
+import useInput from '@/hooks/useInput';
+
+import * as S from './Login.styled';
+
 function Login() {
-  return <h1>로그인 페이지</h1>;
+  const { value: email, setValue: setEmail } = useInput('');
+  const { value: password, setValue: setPassword } = useInput('');
+
+  return (
+    <S.Container>
+      <S.Header>로그인 페이지</S.Header>
+      <S.Form>
+        <S.Label>이름</S.Label>
+        <S.Input type={'text'} value={email} onChange={setEmail} required />
+        <S.Label>비밀번호</S.Label>
+        <S.Input
+          type={'password'}
+          value={password}
+          onChange={setPassword}
+          required
+        />
+        <S.Button type={'submit'}>로그인</S.Button>
+      </S.Form>
+    </S.Container>
+  );
 }
 
 export default Login;
