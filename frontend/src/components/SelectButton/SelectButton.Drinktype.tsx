74ebbecfe958cda20ed11169f5b5d580896aced5
a-mink <ahmin.k95@gmail.com>
2023-05-04 09:57:24 +0900

    fe : selectbutton
    #개요
    
    Select버튼 기능 구현 파일을 Drinktype과 region으로 나눠서 만들었습니다.
    스타일의 경우 styled파일 하나로 Drinktype과 Region파일 두개에 적용했습니다.
    
    #오류
    
    Drinktype과 Region의 기능구현파일에서
    ts(2349) [Ln 41, 열 17] 이 식은 호출할 수 없습니다.
      'StyledComponent<{ theme?: Theme | undefined; as?: any; } & { isOpen: boolean; }, JSX.IntrinsicElements, {}>' 형식에 호출 시그니처가 없습니다.
    라는 오류가 나옵니다.
    
    styled파일에서는
    ts-styled-plugin(9999) [Ln 37, 열 9]
    semi-colon expected
    라는 오류가 나옵니다.
    
    두 오류 모두 해결이 안되어 의견을 구합니다!

---
+++ frontend/src/components/SelectButton/SelectButton.Drinktype.tsx
@@ -0,0 +1,62 @@
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import {
+    Wrapper as wrapperStyles,
+    SelectedOption as selectedOptionStyles,
+    Arrow as arrowStyles,
+    OptionsContainer as optionsContainerStyles,
+    OptionItem as optionItemStyles
+  } from './SelectButton.styled';
+
+const SelectButtonDrinktype = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('술종류');
+
+  const options = [
+    {
+      id: 1,
+      value: '소주',
+    },
+    {
+      id: 2,
+      value: '맥주',
+    },
+        {
+      id: 3,
+      value: '전통주',
+    },
+    {
+      id: 4,
+      value: 'etc',
+    }
+  ];
+
+  const handleOptionClick = (value: string) => {
+    setSelectedOption(value);
+    setIsOpen(false);
+  };
+
+  return (
+    <div css={wrapperStyles}>
+      <div css={selectedOptionStyles(isOpen)} onClick={() => setIsOpen(!isOpen)}>
+        {selectedOption}
+        <div css={arrowStyles}></div>
+      </div>
+      {isOpen && (
+        <div css={optionsContainerStyles}>
+          {options.map((option) => (
+            <div
+              css={optionItemStyles}
+              key={option.id}
+              onClick={() => handleOptionClick(option.value)}
+            >
+              {option.value}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SelectButtonDrinktype;
---
+++ frontend/src/components/SelectButton/SelectButton.styled.tsx
@@ -0,0 +1,69 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  position: relative;
+  width: 150px;
+  height: 50px;
+`;
+
+export const SelectedOption = styled.div<{ isOpen: boolean }>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  padding: 0 10px;
+  font-size: 14px;
+  color: #000000;
+  background-color: #9c94d0;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #a8a2d4;
+  }
+
+  ${props =>
+  props.isOpen &&
+  `
+    background-color: #a8a2d4;
+  `}
+`;
+
+export const Arrow = styled.div`
+  width: 0,
+  height: 0,
+  borderLeft: '5px solid transparent',
+  borderRight: '5px solid transparent',
+  borderTop: '5px solid #fff',
+  right: '10px',
+  top: 'calc(50% - 2.5px)',
+  pointerEvents: 'none',
+`;
+
+export const OptionsContainer = styled.div`
+  position: absolute;
+  top: calc(100% + 5px);
+  left: 0;
+  width: 100%;
+  max-height: 100px;
+  overflow-y: auto;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+  z-index: 999;
+`;
+
+export const OptionItem = styled.div`
+  padding: 10px;
+  font-size: 14px;
+  color: #000000;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #f2f2f2;
+  }
+`;
