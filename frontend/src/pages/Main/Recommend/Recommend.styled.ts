1810bef1a5806b24c0c78900c77a6a7bc3ac1c68
Sin-Sumin <127380267+Sin-Sumin@users.noreply.github.com>
2023-05-04 10:57:54 +0900

    feat: 메인페이지 상품추천영역 구현, remove: 상품카드컴포넌트 및 필요없는데이터(이미지) 삭제, rename: 상품카드 더미데이터 JSON파일 위치 이동
    - 상품추천영역 전체 UI 구현
    - 추천 필터 버튼 구현
    - 상품 카드 캐러셀 구현
    - 상품 카드 컴포넌트 삭제 (해당 내용은 추천영역 컴포넌트로 결합)
    - 상품 카드 별점 영역 데이터 가져오는 기능 추가
    - 필요없는 데이터(이미지) 삭제

---
+++ frontend/src/pages/Main/Recommend/Recommend.styled.ts
@@ -0,0 +1,82 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 80%;
+  height: auto;
+  margin: 0 auto;
+  padding: 7vh 0 10vh;
+`;
+
+const Top = styled.div`
+  width: 78%;
+  height: 20vh;
+  margin: 0 auto;
+`;
+
+const TopLeft = styled.div`
+  width: 80%;
+  overflow: hidden;
+  float: left;
+`;
+
+const Title = styled.h3`
+  margin: 0 0 1.5vh;
+  padding-left: 0.2vw;
+  font-size: 1.5vw;
+`;
+
+const Filter = styled.div`
+  height: auto;
+`;
+
+const ButtonWrapper = styled.div`
+  overflow: hidden;
+`;
+
+const Button = styled.button`
+  border: 1px solid #9C94D0;
+  border-radius: 3vw;
+  box-shadow: 0px 0px 5px #b9b9b9;
+  background-color: white;
+  width: 4.5vw;
+  height: 2vw;
+  font-size: 1vw;
+  display: inline-block;
+  margin-right: 1vw;
+  &:focus{
+    background-color: #9C94D0;
+    color: #fff;
+  }
+`;
+
+const InfoMessageWrapper = styled.div`
+  width: 100%;
+`;
+
+const InfoMessage = styled.p`
+  margin-top: 1vh;
+  padding-left: 0.5vw;
+  font-size: 0.8vw;
+  letter-spacing: 0.05em;
+`;
+
+const TopRight = styled.div`
+  border: 1px solid #b9b9b9;
+  width: 10%;
+  height: 12vh;
+  float: right;
+`;
+
+
+export {
+  Container,
+  Top,
+  TopLeft,
+  Title,
+  Filter,
+  ButtonWrapper,
+  Button,
+  InfoMessageWrapper,
+  InfoMessage,
+  TopRight,
+};
---
+++ frontend/src/pages/Main/Recommend/Recommend.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import * as S from './Recommend.styled';
+import ProductCardCarousel from './ProductCardCarousel/ProductCardCarousel';
+
+const Recommend = () => {
+  const [message, setMessage] = useState('');
+  const [weather, setWeather] = useState('맑은날');
+
+  return (
+    <S.Container>
+      <S.Top>
+        <S.TopLeft>
+          <S.Title>술 추천</S.Title>
+          <S.Filter>
+            <S.ButtonWrapper>
+              <S.Button
+                id="weather"
+                onClick={() => {
+                  setMessage(`${weather}에 가장 인기 있는 술의 리뷰들 입니다!`);
+                }}
+              >
+                날씨
+              </S.Button>
+              <S.Button
+                id="total"
+                onClick={() => {
+                  setMessage('모든 술의 리뷰들 입니다!');
+                }}
+              >
+                전체
+              </S.Button>
+              <S.Button
+                id="weekly"
+                onClick={() => {
+                  setMessage('최근 1주간 가장 인기 있는 술의 리뷰들 입니다!');
+                }}
+              >
+                1주
+              </S.Button>
+              <S.Button
+                id="monthly"
+                onClick={() => {
+                  setMessage('최근 1달간 가장 인기 있는 술의 리뷰들 입니다!');
+                }}
+              >
+                1달
+              </S.Button>
+            </S.ButtonWrapper>
+            <S.InfoMessageWrapper>
+              <S.InfoMessage>{message}</S.InfoMessage>
+            </S.InfoMessageWrapper>
+          </S.Filter>
+        </S.TopLeft>
+        <S.TopRight>날씨 Lottie</S.TopRight>
+      </S.Top>
+      <ProductCardCarousel/>
+    </S.Container>
+  );
+}
+
+
+export default Recommend;
