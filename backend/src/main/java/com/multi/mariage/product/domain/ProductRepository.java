b143f19fc8add008900ad636a8ca81ff0879feb4
rrimm <kmr1033@naver.com>
2023-05-05 14:22:19 +0900

    refactor: 필요없는 코드 삭제

--- backend/src/main/java/com/multi/mariage/product/domain/ProductRepository.java
+++ backend/src/main/java/com/multi/mariage/product/domain/ProductRepository.java
@@ -3,5 +3,4 @@ package com.multi.mariage.product.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product save(Product product);
 }
