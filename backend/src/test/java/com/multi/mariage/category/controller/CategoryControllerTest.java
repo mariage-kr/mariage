d5efe398df48f82fa944db164063b6bf55bba03e
rrimm <kmr1033@naver.com>
2023-05-08 23:48:55 +0900

    test: responseFields 수정

--- backend/src/test/java/com/multi/mariage/category/controller/CategoryControllerTest.java
+++ backend/src/test/java/com/multi/mariage/category/controller/CategoryControllerTest.java
@@ -3,19 +3,14 @@ package com.multi.mariage.category.controller;
 import com.multi.mariage.common.annotation.ControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -28,14 +23,15 @@ class CategoryControllerTest extends ControllerTest {
         mockMvc.perform(get("/api/categories/upper"))
                 .andDo(print())
                 .andDo(document("Category: FindUpperCategory",
-                        preprocessResponse(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
                                 responseFields(
                                         fieldWithPath("category").description("주류 상위 카테고리"),
                                         fieldWithPath("category[].region").description("주류 원산지"),
+                                        fieldWithPath("category[].categories").description("주류 상위 카테고리"),
                                         fieldWithPath("category[].categories[].name").description("주류 상위 카테고리 지칭"),
                                         fieldWithPath("category[].categories[].value").description("주류 상위 카테고리 명칭")
                                 )
-                )
+                        )
                 )
                 .andExpect(status().isOk());
     }
