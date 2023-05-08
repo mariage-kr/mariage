e9086747ef068098f7b99ef65d9f2744db13526c
rrimm <kmr1033@naver.com>
2023-05-08 15:59:41 +0900

    test: 테스트 코드 추가

---
+++ backend/src/test/java/com/multi/mariage/category/controller/CategoryControllerTest.java
@@ -0,0 +1,58 @@
+package com.multi.mariage.category.controller;
+
+import com.multi.mariage.auth.controller.AuthController;
+import com.multi.mariage.category.domain.DrinkUpperCategory;
+import com.multi.mariage.category.dto.DrinkUpperCategoriesResponse;
+import com.multi.mariage.category.dto.DrinkUpperCategoryResponse;
+import com.multi.mariage.category.dto.DrinkUpperCategoryValuesResponse;
+import com.multi.mariage.category.service.DrinkUpperCategoryService;
+import com.multi.mariage.common.annotation.ControllerTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+class CategoryControllerTest extends ControllerTest {
+    AutoCloseable openMocks;
+    protected MediaType contentType =
+            new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
+
+    @BeforeEach
+    void setUp() {
+        openMocks = MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(CategoryController.class).build();
+    }
+
+    @DisplayName("상위 카테고리 조회 테스트")
+    @Test
+    void 상위_카테고리_조회() throws Exception {
+        DrinkUpperCategoryResponse response = Mockito.mock(DrinkUpperCategoryResponse.class);
+        Mockito.when(drinkUpperCategoryService.findCategories()).thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/categories/upper"))
+                .contentType(MediaType.APPLICATION_JSON) //보내는 데이터의 타입을 명시
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(document("categoryList",
+                                preprocessResponse(prettyPrint())
+                        )
+                );
+
+    }
+
+}
--- backend/src/test/java/com/multi/mariage/category/service/DrinkUpperCategoryServiceTest.java
+++ backend/src/test/java/com/multi/mariage/category/service/DrinkUpperCategoryServiceTest.java
@@ -1,28 +1,67 @@
 package com.multi.mariage.category.service;
 
-
+import com.multi.mariage.category.domain.Region;
+import com.multi.mariage.category.dto.DrinkUpperCategoriesResponse;
+import com.multi.mariage.category.dto.DrinkUpperCategoryResponse;
+import com.multi.mariage.category.dto.DrinkUpperCategoryValuesResponse;
 import com.multi.mariage.common.annotation.ServiceTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DrinkUpperCategoryServiceTest extends ServiceTest {
+class DrinkUpperCategoryServiceTest {
 
-    @DisplayName("상위 카테고리 조회 테스트")
+    DrinkUpperCategoryService drinkUpperCategoryService = new DrinkUpperCategoryService();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @DisplayName("모든 지역에 대한 상위 카테고리 조회 테스트")
     @Test
-    void uppercategoryfindTest() {
-        /*
-        given
-        */
+    void 상위카테고리_조회() {
 
-        /*
-        when
-        */
+        DrinkUpperCategoryResponse response = drinkUpperCategoryService.findCategories();
 
-        /*
-        then
-        */
+        assertNotNull(response);
+        List<DrinkUpperCategoryValuesResponse> actual = response.getCategory();
 
+        assertNotNull(actual);
+        assertEquals(Region.values().length, actual.size());
+
+        for (DrinkUpperCategoryValuesResponse categoryValue : actual) {
+            assertNotNull(categoryValue.getRegion());
+            assertNotNull(categoryValue.getCategories());
+            assertFalse(categoryValue.getCategories().isEmpty());
+        }
+    }
+    @DisplayName("특정 지역에 대한 상위 카테고리 조회 테스트")
+    @Test
+    void 특정_지역_필터링_상위카테고리_조회() {
+
+        DrinkUpperCategoryResponse response = drinkUpperCategoryService.findCategories();
+        String region = "국내";
+
+        assertNotNull(response);
+        List<DrinkUpperCategoryValuesResponse> categoryValues = response.getCategory();
+        assertNotNull(categoryValues);
+
+        DrinkUpperCategoryValuesResponse categoryValue = categoryValues.stream()
+                .filter(cv -> region.equals(cv.getRegion()))
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(categoryValue);
+        assertEquals(region, categoryValue.getRegion());
+
+        List<DrinkUpperCategoriesResponse> actual = categoryValue.getCategories();
+        assertFalse(actual.isEmpty());
+
+        for (DrinkUpperCategoriesResponse category : actual) {
+            assertNotNull(category.getValue());
+            assertTrue(category.getValue().contains(region));
+        }
     }
 }
