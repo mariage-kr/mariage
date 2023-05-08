bb092288561eb42fc04e24559cbb4870f81f24d8
rrimm <kmr1033@naver.com>
2023-05-09 03:20:53 +0900

    refactor: 사용하지 않는 코드 삭제

--- backend/src/main/java/com/multi/mariage/country/domain/CountryRepository.java
+++ backend/src/main/java/com/multi/mariage/country/domain/CountryRepository.java
@@ -3,5 +3,4 @@ package com.multi.mariage.country.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
-    Country findByName(Value name);
 }
