f91f6833a11bee0ace2ce232e152b7491a147323
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-09 16:46:24 +0900

    feat: select 옵션 선택 기능 구현

--- frontend/src/pages/Main/SearchFilter/SearchFilter.tsx
+++ frontend/src/pages/Main/SearchFilter/SearchFilter.tsx
@@ -1,10 +1,61 @@
-import * as S from './SearchFilter.styled';
+import { useEffect, useState } from 'react';
 
 import SelectBox from './SelectBox/SelectBox';
 import RangeMultiSlider_M_Star from '@/components/RangeMultiSlider_M/RangeMultiSlider_M_Star';
 import RangeMultiSlider_M_ABV from '@/components/RangeMultiSlider_M/RangeMultiSlider_M_ABV';
 
+import * as S from './SearchFilter.styled';
+
+type Option = {
+  region: string;
+  category: string;
+  rate: Range;
+  level: Range;
+};
+
+type Range = {
+  max: number;
+  min: number;
+};
+
 function SearchFilter() {
+  /**
+   * useState object 형태 업데이트하기
+   * https://jaddong.tistory.com/entry/useState-object-%ED%98%95%ED%83%9C-%EC%97%85%EB%8D%B0%EC%9D%B4%ED%8A%B8%ED%95%98%EA%B8%B0
+   */
+  const [option, setOption] = useState<Option>({
+    region: '',
+    category: '',
+    rate: {
+      max: 0,
+      min: 0,
+    },
+    level: {
+      max: 0,
+      min: 0,
+    },
+  });
+
+  const changeRegionOption = (selectRegion: string) => {
+    setOption({ ...option, region: selectRegion });
+  };
+
+  const changeCategoryOption = (selectCategory: string) => {
+    setOption({ ...option, category: selectCategory });
+  };
+
+  const changeRateOption = (selectRateRange: Range) => {
+    setOption({ ...option, rate: selectRateRange });
+  };
+
+  const changeLevelOption = (selectLevelRange: Range) => {
+    setOption({ ...option, level: selectLevelRange });
+  };
+
+  useEffect(() => {
+    console.log(option);
+  }, [option]);
+
   return (
     <S.Container>
       <S.Wrapper>
@@ -12,7 +63,12 @@ function SearchFilter() {
           <S.Left>
             <S.Drinks>
               <S.FilterTitle>주종</S.FilterTitle>
-              <SelectBox />
+              <SelectBox
+                changeRegion={(region: string) => changeRegionOption(region)}
+                changeCategory={(category: string) =>
+                  changeCategoryOption(category)
+                }
+              />
             </S.Drinks>
           </S.Left>
           <S.Right>
--- frontend/src/pages/Main/SearchFilter/SelectBox/SelectBox.tsx
+++ frontend/src/pages/Main/SearchFilter/SelectBox/SelectBox.tsx
@@ -1,41 +1,60 @@
 import { useEffect, useState } from 'react';
-import * as S from './SelectBox.styled';
+import { useRecoilValue } from 'recoil';
 
 import useSelect from '@/hooks/useSelect';
-import { useRecoilValue } from 'recoil';
-import { drinkUpperCategoryState } from '@/store/status';
 import { CategoryType } from '@/types/category';
+import { drinkUpperCategoryState } from '@/store/status';
 
-interface Options {
-  value: string;
-  name: string;
-}
+import * as S from './SelectBox.styled';
 
-function SelectBox() {
+/**
+ * 1. [Typescript] React - useState를 props로 전달할 때의 타입 선언
+ * https://velog.io/@rkio/Typescript-React-useState%EB%A5%BC-props%EB%A1%9C-%EC%A0%84%EB%8B%AC%ED%95%A0-%EB%95%8C%EC%9D%98-%ED%83%80%EC%9E%85-%EC%84%A0%EC%96%B8
+ *
+ * 2. React + Typescript 프롭스 타입 정의 하기
+ * https://velog.io/@ovogmap/React-Typescript-2
+ *
+ * 3. typescript에서 undefined value 처리하기
+ * https://blog.toycrane.xyz/typescript%EC%97%90%EC%84%9C-undefined-value-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0-181035b5ee47
+ */
+
+type Props = {
+  changeRegion(region: string): void;
+  changeCategory(category: string): void;
+};
+
+function SelectBox({ changeRegion, changeCategory }: Props) {
   const drinkUpperCategory = useRecoilValue(drinkUpperCategoryState);
-
-  const { value: option, setValue: setOption } = useSelect(
-    drinkUpperCategory[0].region,
-  );
-
   const [category, setCategory] = useState<CategoryType | undefined>(
     drinkUpperCategory[0],
   );
 
-  const onRegionChangeHandler = (option: string) => {
+  const { value: region, setValue: setRegion } = useSelect<string>(
+    drinkUpperCategory[0].region,
+  );
+  const { value: selectCategory, setValue: setSelectCategory } =
+    useSelect<string>('');
+
+  const onRegionChangeHandler = (region: string) => {
     setCategory(
-      drinkUpperCategory.find(category => category.region === option),
+      drinkUpperCategory.find(category => category.region === region),
     );
   };
 
+  /* TODO: Region 변경시 하위 카테고리도 자동으로 변경하는 로직 작성 */
   useEffect(() => {
-    onRegionChangeHandler(option);
-  }, [option]);
+    changeRegion(region);
+    onRegionChangeHandler(region);
+  }, [region]);
+
+  useEffect(() => {
+    changeCategory(selectCategory);
+  }, [selectCategory]);
 
   return (
     <S.Container>
       <label>
-        <S.SelectBox onChange={setOption}>
+        <S.SelectBox onChange={setRegion}>
           {drinkUpperCategory.map((category: CategoryType, index: number) => (
             <S.Option key={index} value={category.region}>
               {category.region}
@@ -44,10 +63,10 @@ function SelectBox() {
         </S.SelectBox>
       </label>
       <label>
-        <S.SelectBox>
+        <S.SelectBox onChange={setSelectCategory}>
           {category &&
             category.categories.map((category, index) => (
-              <S.Option key={index} value={category.value}>
+              <S.Option key={index} value={category.name}>
                 {category.value}
               </S.Option>
             ))}
