4001f7b9b305549114f43a6a5199d78eb90edc69
rrimm <kmr1033@naver.com>
2023-05-10 13:08:53 +0900

    test: 특정한 하위 카테고리 조회 테스트 추가

--- backend/src/test/java/com/multi/mariage/category/service/DrinkLowerCategoryServiceTest.java
+++ backend/src/test/java/com/multi/mariage/category/service/DrinkLowerCategoryServiceTest.java
@@ -1,13 +1,12 @@
 package com.multi.mariage.category.service;
 
 import com.multi.mariage.category.domain.Region;
-import com.multi.mariage.category.dto.DrinkLowerCategoriesResponse;
-import com.multi.mariage.category.dto.DrinkLowerCategoryResponse;
-import com.multi.mariage.category.dto.DrinkLowerCategoryValuesResponse;
-import com.multi.mariage.category.dto.DrinkUpperCategoryValuesResponse;
+import com.multi.mariage.category.dto.*;
 import com.multi.mariage.common.annotation.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -28,5 +27,36 @@ class DrinkLowerCategoryServiceTest extends ServiceTest {
         for (DrinkLowerCategoryValuesResponse categoryValue : actual) {
             assertThat(categoryValue.getCategories()).isNotEmpty();
         }
+    }
+
+    @DisplayName("특정한 하위 카테고리를 조회한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"소주", "맥주", "전통주", "기타"})
+    void 특정한_하위_카테고리를_조회한다(String lowerCategory) {
+
+        String region = "국내";
+
+        DrinkLowerCategoryResponse response = drinkLowerCategoryService.findCategories();
+        assertThat(response).isNotNull();
+
+        List<DrinkLowerCategoryValuesResponse> categoryValues = response.getCategory();
+        assertThat(categoryValues).isNotEmpty();
+
+        DrinkLowerCategoryValuesResponse categoryValue = categoryValues.stream()
+                .filter(cv -> region.equals(cv.getRegion()))
+                .findFirst()
+                .orElse(null);
+        assertThat(categoryValue).isNotNull();
+
+        List<DrinkLowerCategoriesResponse> subCategoryValues = categoryValue.getCategories();
+        assertThat(subCategoryValues).isNotEmpty();
+
+        DrinkLowerCategoriesResponse subCategoryValue = subCategoryValues.stream()
+                .filter(cv -> lowerCategory.equals(cv.getValue()))
+                .findFirst()
+                .orElse(null);
+
+        assertThat(subCategoryValue).isNotNull();
+        assertThat(subCategoryValue.getValue()).isEqualTo(lowerCategory);
     }
 }
