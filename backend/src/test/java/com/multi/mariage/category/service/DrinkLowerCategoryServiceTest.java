088c0abf2cc0445e8683555bbdef109bb266d3fb
rrimm <kmr1033@naver.com>
2023-05-10 10:17:37 +0900

    test: 하위 카테고리 조회 테스트

---
+++ backend/src/test/java/com/multi/mariage/category/service/DrinkLowerCategoryServiceTest.java
@@ -0,0 +1,34 @@
+package com.multi.mariage.category.service;
+
+import com.multi.mariage.category.domain.Region;
+import com.multi.mariage.category.dto.DrinkLowerCategoriesResponse;
+import com.multi.mariage.category.dto.DrinkLowerCategoryResponse;
+import com.multi.mariage.category.dto.DrinkLowerCategoryValuesResponse;
+import com.multi.mariage.category.dto.DrinkUpperCategoryValuesResponse;
+import com.multi.mariage.common.annotation.ServiceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrinkLowerCategoryServiceTest extends ServiceTest {
+    DrinkLowerCategoryService drinkLowerCategoryService = new DrinkLowerCategoryService();
+
+    @DisplayName("하위 카테고리를 조회한다.")
+    @Test
+    void 하위_카테고리를_조회한다() {
+        DrinkLowerCategoryResponse response = drinkLowerCategoryService.findCategories();
+
+        assertThat(response).isNotNull();
+        assertThat(response.getCategory()).isNotNull().hasSize(Region.values().length);
+        List<DrinkLowerCategoryValuesResponse> actual = response.getCategory();
+
+        for (DrinkLowerCategoryValuesResponse categoryValue : actual) {
+            assertThat(categoryValue.getRegion()).isNotNull();
+            assertThat(categoryValue.getCategories()).isNotEmpty();
+        }
+    }
+}
