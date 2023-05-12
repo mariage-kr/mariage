ec0bec102224638349f97aaffd60473d60af86c6
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-12 09:40:42 +0900

    refactor: 타입 이름 변경
    타입의 이름을 변경한 뒤 어떠한 역활을 하는지 주석을 작성

--- frontend/src/types/category.ts
+++ frontend/src/types/category.ts
@@ -1,26 +1,46 @@
 /* 메인 검색 필터 */
 interface CategoryType {
   region: string;
-  categories: CategoriesType[];
+  categories: DrinkLowerCategoryType[];
 }
 
-interface CategoriesType {
+/**
+ * 주류의 지역별 카테고리
+ * @property {string} region - 지역의 이름
+ * @property {string} regionValue - 지역의 데이터
+ * @property {LowerCategoryType[]} drinkUpperCategory - 해당 지역의 주류의 하위 카테고리
+ */
+interface DrinkRegionCategoryType {
+  region: string;
+  regionValue: string;
+  categories: DrinkUpperCategoryType[];
+}
+
+/**
+ * 주류의 상위 카테고리
+ * @property {string} name - 주류의 상위 카테고리 데이터
+ * @property {string} value - 주류의 상위 카테고리 이름
+ * @property {CategoryType[]} drinkLowerCategory - 해당 주류의 하위 카테고리
+ */
+interface DrinkUpperCategoryType {
+  name: string;
+  value: string;
+  subCategories: DrinkLowerCategoryType[];
+}
+
+/**
+ * 주류의 하위 카테고리
+ * @property {string} value - 주류의 이름
+ * @property {string} name - 주류의 데이터
+ */
+interface DrinkLowerCategoryType {
   value: string;
   name: string;
 }
 
-/* 제품 등록 */
-
-interface UpperCategoryType {
-  region: string; // example: "국내"
-  regionValue: string; // example: "LOCAL"
-  categories: LowerCategoryType[];
-}
-
-interface LowerCategoryType {
-  name: string; // example: "LOCAL_SOJU"
-  value: string; // example: "소주"
-  subCategories: CategoriesType[];
-}
-
-export { CategoryType, CategoriesType, UpperCategoryType, LowerCategoryType };
+export {
+  CategoryType,
+  DrinkRegionCategoryType,
+  DrinkUpperCategoryType,
+  DrinkLowerCategoryType,
+};
