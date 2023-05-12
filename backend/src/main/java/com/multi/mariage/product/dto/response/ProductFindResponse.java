4b495da5a0077b5acc944a13cbb894fd5c2cbbc1
rrimm <kmr1033@naver.com>
2023-05-12 20:59:47 +0900

    feat: 상품 조회 기능 추가

---
+++ backend/src/main/java/com/multi/mariage/product/dto/response/ProductFindResponse.java
@@ -0,0 +1,30 @@
+package com.multi.mariage.product.dto.response;
+
+import com.multi.mariage.product.vo.ProductsVO;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+public class ProductFindResponse {
+    private List<ProductsVO> product;
+    private int length;
+
+    @Builder
+    private ProductFindResponse(List<ProductsVO> product, int length) {
+        this.product = product;
+        this.length = length;
+    }
+
+    public static ProductFindResponse from(List<ProductsVO> product) {
+        return ProductFindResponse.builder()
+                .product(product)
+                .length(product.size())
+                .build();
+    }
+
+}
