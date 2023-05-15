de2179d12d81c731d5918a62dacc17516e59ff36
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-15 11:29:08 +0900

    feat: 제품 조회 SQL 작성

---
+++ backend/src/main/java/com/multi/mariage/product/domain/query/ProductRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.multi.mariage.product.domain.query;
+
+import com.multi.mariage.product.domain.Product;
+
+import java.util.List;
+
+public interface ProductRepositoryCustom {
+    List<Product> searchProductByName(String name);
+}
