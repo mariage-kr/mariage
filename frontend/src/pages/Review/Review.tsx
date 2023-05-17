1f5a1f675c444ac60e9a233106bb77df8eabdc9a
Sin-Sumin <127380267+Sin-Sumin@users.noreply.github.com>
2023-05-17 16:39:59 +0900

    feat: 리뷰페이지 구현
    - 리뷰페이지 전체 UI 구현 (미디어쿼리 완료)
    - 리뷰 컴포넌트 구현 (미디어쿼리 완료)

--- frontend/src/pages/Review/Review.tsx
+++ frontend/src/pages/Review/Review.tsx
@@ -1,8 +1,48 @@
+import ReviewList from '@/components/Review/ReviewList'
+import * as S from './Review.styled';
+
 function Review() {
+  const profileData = {
+    id: 1,
+    img: 'https://i.esdrop.com/d/f/CeyD9bnnT5/K86nd4Er00.png',
+    nickname: '마리아',
+    email: '#mariage@google.com',
+    reviews: 1042,
+    likes: 12301
+  };
+
+  // toLocaleString();
+
   return (
-  <>
-  <h1>리뷰페이지</h1>
-  </>
+    <S.Container>
+      <S.TopNav>
+        <S.NavWrapper>
+          <S.Nav>작성한 리뷰</S.Nav>
+          <S.Nav>좋아요한 리뷰</S.Nav>
+        </S.NavWrapper>
+      </S.TopNav>
+      <S.Main>
+        <S.Profile>
+          <S.ProfileLeft><S.ProfileImg src={profileData.img}/></S.ProfileLeft>
+          <S.ProfileRight>
+            <S.NameEmail css={S.name}>{profileData.nickname}</S.NameEmail>
+            <S.NameEmail css={S.email}>{profileData.email}</S.NameEmail>
+            <S.Reviews>
+              <S.Title>Reviews</S.Title>
+              <S.Count>{profileData.reviews}</S.Count>
+            </S.Reviews>
+            <S.Likes>
+              <S.Title>Likes</S.Title>
+              <S.Count>{profileData.likes}</S.Count>
+            </S.Likes>
+          </S.ProfileRight>
+        </S.Profile>
+      </S.Main>
+      <ReviewList/>
+      <ReviewList/>
+      <ReviewList/>
+      <ReviewList/>      
+    </S.Container>
   );
 }
 
