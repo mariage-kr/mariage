0e16d08f0e049dfdeab8b6ee9d304eb07798a32c
a-mink <ahmin.k95@gmail.com>
2023-05-18 13:43:51 +0900

    Saving changes before merge

--- backend/src/main/java/com/multi/mariage/review/dto/response/ReviewLikeResponse.java
+++ backend/src/main/java/com/multi/mariage/review/dto/response/ReviewLikeResponse.java
@@ -1,6 +1,6 @@
 package com.multi.mariage.review.dto.response;
 
-import com.multi.mariage.product.vo.ReviewVO;
+import com.multi.mariage.review.vo.ReviewVO;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -8,5 +8,24 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@NoArgsConstructor(access = Access.Level.PROTECTED)
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class ReviewLikeResponse {
+
+    private List<ReviewVO> review;
+    private int length;
+
+    @Builder
+    private ReviewLikeResponse(List<ReviewVO> review, int length) {
+        this.review = review;
+        this.length = length;
+    }
+
+    public static ReviewLikeResponse from(List<ReviewVO> review) {
+        return ReviewLikeResponse.builder()
+                .review(review)
+                .length(review.size())
+                .build();
+    }
+}
