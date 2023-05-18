d8740fe8e10da2f6b23358179ea80584b7104d89
rrimm <kmr1033@naver.com>
2023-05-18 13:48:41 +0900

    test: 제품 삭제 테스트 코드 작성

--- backend/src/test/java/com/multi/mariage/product/service/ProductModifyServiceTest.java
+++ backend/src/test/java/com/multi/mariage/product/service/ProductModifyServiceTest.java
@@ -5,14 +5,15 @@ import com.multi.mariage.common.fixture.ProductFixture;
 import com.multi.mariage.product.domain.Product;
 import com.multi.mariage.product.dto.request.ProductSaveRequest;
 import com.multi.mariage.storage.domain.Image;
+import com.multi.mariage.storage.service.ImageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.parameters.P;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductModifyServiceTest extends ServiceTest {
-
     @BeforeEach
     void setUp() {
         Image image = new Image(ProductFixture.참이슬.getImageName());
@@ -32,5 +33,22 @@ class ProductModifyServiceTest extends ServiceTest {
         Product actual = productModifyService.save(saveRequest);
 
         assertThat(actual).isNotNull();
+    }
+
+    @DisplayName("식별자를 통해 제품을 삭제한다.")
+    @Test
+    void 식별자를_통해_제품을_삭제한다() {
+        Product product = save();
+
+        productModifyService.remove(product.getId());
+
+    }
+
+    private Product save() {
+        Image savedImage = storageRepository.save(new Image(ProductFixture.처음처럼.getImageName()));
+        ProductSaveRequest saveRequest = ProductFixture.처음처럼.toProductSaveRequest(savedImage.getId());
+
+        Product actual = productModifyService.save(saveRequest);
+        return actual;
     }
 }
