fb7660c9edb03ec29bcf96c3b092ee5f19db3329
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-19 22:31:46 +0900

    chore: image type 설정

--- frontend/src/@types/image.d.ts
+++ frontend/src/@types/image.d.ts
@@ -1,4 +1,7 @@
+declare module '*.jpg';
 declare module '*.png';
+declare module '*.jpeg';
+declare module '*.gif';
 declare module '*.svg' {
   import React = require('react');
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
