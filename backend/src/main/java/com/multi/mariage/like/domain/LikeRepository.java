545456de592f46b1a3227a568cd49b5370ddbc4f
rrimm <kmr1033@naver.com>
2023-05-21 14:56:23 +0900

    test: 테스트 코드 수정

--- backend/src/main/java/com/multi/mariage/like/domain/LikeRepository.java
+++ backend/src/main/java/com/multi/mariage/like/domain/LikeRepository.java
@@ -1,8 +1,6 @@
 package com.multi.mariage.like.domain;
 
-import com.multi.mariage.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
