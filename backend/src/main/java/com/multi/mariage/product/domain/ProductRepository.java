c8be1905aefc52e95ce9b08bfd630b2543873f83
rrimm <kmr1033@naver.com>
2023-05-23 20:53:29 +0900

    refactor: 사용하지 않는 코드 삭제

--- backend/src/main/java/com/multi/mariage/product/domain/ProductRepository.java
+++ backend/src/main/java/com/multi/mariage/product/domain/ProductRepository.java
@@ -3,22 +3,7 @@ package com.multi.mariage.product.domain;
 import com.multi.mariage.product.domain.embedded.Name;
 import com.multi.mariage.product.domain.query.ProductRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
     boolean existsByName(Name name);
-
-    @Query("select avg(coalesce(r.foodRate, 0)) " +
-            "from Product p " +
-            "left join p.reviews r " +
-            "where p.id = :productId")
-    Double getTotalFoodRateAverageByProductId(@Param("productId") Long productId);
-
-    @Query("SELECT r.foodCategory, AVG(r.foodRate) " +
-            "FROM Review r " +
-            "GROUP BY r.foodCategory")
-    List<Object[]> getAverageFoodRates();
 }
