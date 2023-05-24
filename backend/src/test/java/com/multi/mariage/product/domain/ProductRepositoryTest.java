3d051ad69032ed44d0333c076b897687ed6e553c
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-24 16:11:24 +0900

    test: Repository existsByName 테스트 코드 작성

--- backend/src/test/java/com/multi/mariage/product/domain/ProductRepositoryTest.java
+++ backend/src/test/java/com/multi/mariage/product/domain/ProductRepositoryTest.java
@@ -2,7 +2,7 @@ package com.multi.mariage.product.domain;
 
 import com.multi.mariage.common.annotation.RepositoryTest;
 import com.multi.mariage.common.fixture.ProductFixture;
-import com.multi.mariage.storage.domain.Image;
+import com.multi.mariage.product.domain.embedded.Name;
 import com.multi.mariage.storage.repository.StorageRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -35,5 +35,15 @@ class ProductRepositoryTest extends RepositoryTest {
     void 제품을_저장한다() {
         Product actual = saveProduct(ProductFixture.산토리_위스키);
         assertThat(actual).isNotNull();
+    }
+
+    @DisplayName("제품이름이 존재하는지 조회한다.")
+    @Test
+    void 제품이름이_존재하는지_조회한다() {
+        Name name = Name.of(참이슬.getName());
+
+        boolean actual = productRepository.existsByName(name);
+
+        assertThat(actual).isTrue();
     }
 }
