591cf93fb935a05435ee27ddd2037779e5567a38
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-05-29 18:29:35 +0900

    feat: 제품 작성시 해당 페이지로 이동

--- backend/src/main/java/com/multi/mariage/product/controller/ProductModifyController.java
+++ backend/src/main/java/com/multi/mariage/product/controller/ProductModifyController.java
@@ -3,7 +3,7 @@ package com.multi.mariage.product.controller;
 import com.multi.mariage.product.domain.Product;
 import com.multi.mariage.product.dto.request.ProductSaveRequest;
 import com.multi.mariage.product.dto.request.ProductUpdateRequest;
-import com.multi.mariage.product.dto.response.ProducModifyResponse;
+import com.multi.mariage.product.dto.response.ProductModifyResponse;
 import com.multi.mariage.product.service.ProductModifyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +18,16 @@ public class ProductModifyController {
     private final ProductModifyService productModifyService;
 
     @PostMapping("/user/product/save")
-    public ResponseEntity<ProducModifyResponse> save(@RequestBody @Valid ProductSaveRequest request) {
+    public ResponseEntity<ProductModifyResponse> save(@RequestBody @Valid ProductSaveRequest request) {
         Product product = productModifyService.save(request);
-        ProducModifyResponse response = new ProducModifyResponse(product.getId());
+        ProductModifyResponse response = new ProductModifyResponse(product.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PatchMapping("/user/product/update")
-    public ResponseEntity<ProducModifyResponse> update(@RequestBody ProductUpdateRequest request) {
+    public ResponseEntity<ProductModifyResponse> update(@RequestBody ProductUpdateRequest request) {
         Product product = productModifyService.update(request);
-        ProducModifyResponse response = new ProducModifyResponse(product.getId());
+        ProductModifyResponse response = new ProductModifyResponse(product.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
