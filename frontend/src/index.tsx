5ffa3ad642cc2c48394423a44d834286c246e9a7
Sin-Sumin <127380267+Sin-Sumin@users.noreply.github.com>
2023-05-30 11:44:26 +0900

    feat: 제품페이지 제품리스트 무한스크롤 구현
    제품리스트 무한스크롤

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import worker from './mocks/browsers';
-
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(<App />);
