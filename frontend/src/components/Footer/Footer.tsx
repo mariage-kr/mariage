fa43f9eb0319a944e4d304045a8496b8d3fe8cce
rrimm <kmr1033@naver.com>
2023-06-01 15:22:35 +0900

    refactor: 사용하지 않는 코드 삭제

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -27,11 +27,6 @@ function Footer() {
     setIsMain(window.scrollY === 0);
   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
     <>
       <S.Container>
